1a73bb2956bf43351079082d720082718f506938
sayed6201 <sayed.ahmed620@gmail.com>
2023-01-04 15:57:04 +0000

    node snippets

---
+++ Node-Express/image-upload.js
@@ -0,0 +1,8 @@
+// Next, install express as a framework,ejs as a template engine, 
+// body-parser, multer as a  node.js middleware for handling multipart/form-data, 
+// uuid for getting the random name, 
+// sharp as an image resize libraries using NPM.
+
+//---------------------------------------------------------------------------------
+// npm install express body-parser ejs uuid multer sharp --save
+//---------------------------------------------------------------------------------
